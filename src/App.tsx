6c760eca6043f608d6269db94a0cc1c98f0864f6
Grzegorz <grzegorzkaw@gmail.com>
2023-05-24 02:16:54 +0200

    Added fetch functionality, added tailwind.css, changed styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css'
 import Form from './components/Form'
 
 function App() {
 
   return (
-   <div>
+   <div className='p-4 flex justify-center items-center h-[100vh] bg-black text-white'>
     <Form />
    </div>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -13,135 +13,188 @@ interface IFormInputs {
 }
 
 export default function Form() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFormInputs>();
-  const onSubmit = (data: IFormInputs) => {console.log(data)}
   const [isPizza, setIsPizza] = useState(true);
   const [isSoup, setIsSoup] = useState(false);
   const [isSandwich, setIsSandwich] = useState(false);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <ErrorMessage errors={errors} as="p" name="name" />
-      <input
-        type="text"
-        placeholder="name"
-        {...register("name", { required: "Please provide a name" })}
-      />
-      <ErrorMessage errors={errors} as="p" name="preparation_time" />
-      <input
-        type="text"
-        placeholder="preparation_time"
-        {...register("preparation_time", {
-          required: { value: true, message: "prep time is required" },
-          pattern: {
-            value: /[0-9]{2}:[0-9]{2}:[0-9]{2}/i,
-            message: "Please provide an correct input 00:00:00",
+  const {
+    register,
+    unregister,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>();
+  const onSubmit = async (data: IFormInputs) => {
+    console.log(data);
+    try {
+      const response = await fetch(
+        "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-        })}
-      />
-      <ErrorMessage errors={errors} as="p" name="type" />
-      <select
-        {...register("type", {
-          required: { value: true, message: "Please Select An option" },
-        })}
-        onChange={(e) => {
-          {
-            if (e.target.value === "pizza") {
-              setIsPizza(true);
-              setIsSandwich(false);
-              setIsSoup(false);
-            }
-            if (e.target.value === "soup") {
-              setIsSoup(true);
-              setIsPizza(false);
-              setIsSandwich(false);
-            }
-            if (e.target.value === "sandwich") {
-              setIsSandwich(true);
-              setIsPizza(false);
-              setIsSoup(false);
-            }
-          }
-        }}
-      >
-        <option value="pizza">Pizza</option>
-        <option value="soup">Soup</option>
-        <option value="sandwich">Sandwich</option>
-      </select>
-      {isPizza ? (
-        <div className="pizza-div">
-          <ErrorMessage errors={errors} as="p" name="no_of_slices" />
-          <input
-            type="number"
-            placeholder="no_of_slices"
-            {...register("no_of_slices", {
-              required: {
-                value: isPizza ? true : false,
-                message: "please set a number",
-              },
-              min: { value: 1, message: "pleace" },
-            })}
-          />
-          <ErrorMessage errors={errors} as="p" name="diameter" />
-          <input
-            type="number"
-            step={0.01}
-            placeholder="diameter"
-            {...register("diameter", {
-              required: {
-                value: isPizza ? true : false,
-                message: "please set a number",
-              },
-              min: { value: 0.01, message: "pleace" },
-            })}
-          />
-        </div>
-      ) : (
-        ""
-      )}
-      {isSoup ? (
-        <div>
-          <p>How spicy would you like it?</p>
-          <input
-            type="range"
-            min="0"
-            max="10"
-            step="1"
-            placeholder="spiciness_scale"
-            {...register("spiciness_scale", {
-              required: isSoup ? true : false,
-            })}
-          />
-        </div>
-      ) : (
-        ""
-      )}
-      {isSandwich ? (
-        <div>
-          {" "}
-          <ErrorMessage errors={errors} as="p" name="slices_of_bread" />
-          <input
-            type="number"
-            placeholder="slices_of_bread"
-            {...register("slices_of_bread", {
-              required: {
-                value: isSandwich ? true : false,
-                message: "please choose a number of slices of bread",
-              },
-              min: { value: 1, message: "pleace" },
-            })}
-          />
-        </div>
-      ) : (
-        ""
-      )}
-      <div className="errors"></div>
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-      <input type="submit" />
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 justify-center items-center p-12 w-[75%] h-[50%] bg-white rounded-xl text-black"
+    >
+      <div className="flex flex-col">
+        <div className="p-4 flex gap-8">
+          <input
+            type="text"
+            placeholder="name"
+            {...register("name", { required: "Please provide a name", minLength: {value:3, message:"The name needs to be at least 3 characters"} })}
+          />
+          <ErrorMessage errors={errors} name="name" />
+        </div>
+        <div className="p-4 flex gap-8">
+          <input
+            type="text"
+            placeholder="preparation_time"
+            max={8}
+            {...register("preparation_time", {
+              required: { value: true, message: "prep time is required" },
+              pattern: {
+                value: /[0-9]{2}:[0-9]{2}:[0-9]{2}/i,
+                message: "Please provide an correct input 00:00:00",
+              },
+              maxLength: {
+                value: 8,
+                message: "Please provide an correct input 00:00:00",
+              },
+            })}
+          />
+          <ErrorMessage errors={errors} name="preparation_time" />
+        </div>
+        <div className="p-4 flex gap-8 justify-center items-center">
+          <select
+            {...register("type", {
+              required: { value: true, message: "Please Select An option" },
+            })}
+            onChange={(e) => {
+              {
+                if (e.target.value === "pizza") {
+                  setIsPizza(true);
+
+                  setIsSandwich(false);
+                  unregister("slices_of_bread");
+
+                  setIsSoup(false);
+                  unregister("spiciness_scale");
+                }
+                if (e.target.value === "soup") {
+                  setIsSoup(true);
+
+                  setIsPizza(false);
+                  unregister("no_of_slices");
+                  unregister("diameter");
+
+                  setIsSandwich(false);
+                  unregister("slices_of_bread");
+                }
+                if (e.target.value === "sandwich") {
+                  setIsSandwich(true);
+
+                  setIsPizza(false);
+                  unregister("no_of_slices");
+                  unregister("diameter");
+
+                  setIsSoup(false);
+                  unregister("spiciness_scale");
+                }
+              }
+            }}
+          >
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="sandwich">Sandwich</option>
+          </select>
+          <ErrorMessage errors={errors} name="type" />
+        </div>
+      </div>
+      <div>
+        {isPizza ? (
+          <div className="flex flex-col">
+            <div className="p-4 flex gap-8">
+              <input
+                type="number"
+                placeholder="no_of_slices"
+                {...register("no_of_slices", {
+                  required: {
+                    value: isPizza ? true : false,
+                    message: "please set a number",
+                  },
+                  min: { value: 1, message: "pleace" },
+                })}
+              />
+              <ErrorMessage errors={errors} name="no_of_slices" />
+            </div>
+            <div className="p-4 flex gap-8">
+              <input
+                type="number"
+                step={0.01}
+                placeholder="diameter"
+                {...register("diameter", {
+                  required: {
+                    value: isPizza ? true : false,
+                    message: "please set a number",
+                  },
+                  min: { value: 0.01, message: "pleace" },
+                })}
+              />
+              <ErrorMessage errors={errors} name="diameter" />
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        {isSoup ? (
+          <div className="p-4 flex flex-col gap-8">
+            <p>How spicy would you like it?</p>
+            <input
+              type="range"
+              min="0"
+              max="10"
+              step="1"
+              placeholder="spiciness_scale"
+              {...register("spiciness_scale", {
+                required: isSoup ? true : false,
+              })}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+        {isSandwich ? (
+          <div className="p-4 flex gap-8">
+            <input
+              type="number"
+              placeholder="slices_of_bread"
+              {...register("slices_of_bread", {
+                required: {
+                  value: isSandwich ? true : false,
+                  message: "please choose a number of slices of bread",
+                },
+                min: { value: 1, message: "pleace" },
+              })}
+            />
+            <ErrorMessage errors={errors} name="slices_of_bread" />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <input type="submit" value="Submit"/>
     </form>
   );
 }
